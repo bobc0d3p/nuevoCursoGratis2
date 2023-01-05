1b9d584eb367c14e2d7881bb2f5936074977da78
robert daroch <bob.c0d3p@gmail.com>
2023-01-05 15:17:09 -0300

    septimo commit

--- js/mokepon.js
+++ js/mokepon.js
@@ -33,7 +33,7 @@ function seleccionarMascotaJugador() {
     let sectionSeleccionarMascota = document.getElementById('seleccionar-mascota')
     sectionSeleccionarMascota.style.display = 'none'
     let sectionSeleccionarAtaque = document.getElementById('seleccionar-ataque')
-    sectionSeleccionarAtaque.style.display = 'block'
+    sectionSeleccionarAtaque.style.display = 'flex'
     
     let inputHipodoge = document.getElementById('hipodoge')
     let inputCapipepo = document.getElementById('capipepo')
